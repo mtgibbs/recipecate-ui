eb31055065f087128ff02d92247b844698179af3
Matt Gibbs <matt@mtgibbs.xyz>
2019-01-03 21:34:22 -0500

    Adding pages and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +9,19 @@ import { RecipesPageComponent } from './components/recipes-page/recipes-page.com
 import { ApiModule } from './api/api.module';
 import { AngularMaterialModule } from './shared/angular-material.module';
 import { RecipeDetailPageComponent } from './components/recipe-detail-page/recipe-detail-page.component';
-import { RecipesResolver, RecipeDetailResolver, IngredientsResolver } from './resolvers';
+import { RecipesResolver, RecipeDetailResolver, IngredientsResolver, MealPlanDetailResolver, MealPlansResolver } from './resolvers';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecipeCreatePageComponent } from './components/recipe-create-page/recipe-create-page.component';
+import { MealPlansPageComponent } from './components/meal-plans-page/meal-plans-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesPageComponent,
     RecipeDetailPageComponent,
-    RecipeCreatePageComponent
+    RecipeCreatePageComponent,
+    MealPlansPageComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,9 @@ import { RecipeCreatePageComponent } from './components/recipe-create-page/recip
   providers: [
     RecipesResolver,
     RecipeDetailResolver,
-    IngredientsResolver
+    IngredientsResolver,
+    MealPlanDetailResolver,
+    MealPlansResolver
   ],
   bootstrap: [AppComponent]
 })
