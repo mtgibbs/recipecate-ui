3a7ba9e6e31fa805b0661e2bf0fcb457586692a8
Matt Gibbs <matt@mtgibbs.xyz>
2019-01-15 21:23:32 -0500

    Working on local storage test

--- src/app/resolvers/unit-of-measurement.resolver.ts
+++ src/app/resolvers/unit-of-measurement.resolver.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IngredientsService } from '../api/services';
 import { UnitsOfMeasurementList } from '../api/models';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class UnitOfMeasurementResolver implements Resolve<UnitsOfMeasurementList> {
 
     constructor(
-        private ingredientsService: IngredientsService) { }
+        private ingredientsService: IngredientsService,
+        protected localStorage: LocalStorage) { }
 
     resolve(route: ActivatedRouteSnapshot): Observable<UnitsOfMeasurementList> {
-        return this.ingredientsService.getIngredientsUnitsofmeasurement();
+
+        return this.localStorage.getItem<UnitsOfMeasurementList>('units_of_measurement').pipe(
+            mergeMap((list: UnitsOfMeasurementList) => {
+
+                if (!list || list.length === 0) {
+
+                    return this.ingredientsService.getIngredientsUnitsofmeasurement().pipe(
+                        mergeMap(resultList => {
+                            this.localStorage.setItem('units_of_measurement', resultList);
+                            return of(resultList);
+                        })
+                    );
+                }
+
+                return of(list);
+            })
+        );
     }
 }
